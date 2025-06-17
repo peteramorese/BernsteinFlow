04799522e15435610eb214400f19d8c4858db285
Peter Amorese <peter.amorese@gmail.com>
2025-06-16 19:50:36 -0600

    Functions, but known issue with softmax clipping at .5

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -5,6 +5,7 @@ from .TestDataSets import sample_modal_gaussian, plot_data
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -41,41 +42,87 @@ def evaluate_density_on_grid(model, resolution=100, device=None):
     Z = densities.reshape(resolution, resolution)
     return X, Y, Z
 
-def plot_density(ax, X, Y, Z):
-    ax.countourf(X, Y, Z, levels=50, cmap='viridis')
+def plot_density_surface(X, Y, Z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    # Create surface plot
+    surf = ax.plot_surface(X, Y, Z, cmap='viridis', linewidth=0, antialiased=True)
+    # Add colorbar
+    fig.colorbar(surf, ax=ax, shrink=0.5, aspect=20)
+    return fig, ax
+
+def plot_density(ax : Axes3D, X, Y, Z):
+    ax.contourf(X, Y, Z, levels=50, cmap='viridis')
 
 
 dim = 2
 
 gdt = GaussianDistTransform(mean=dim*[0.0], variances=dim*[5.0])
 
-means = [[-1.5, -1.5], [1.5, 1.5]]
-covariances = [torch.eye(dim)*1.5, torch.eye(dim)*0.5]
-X_data = sample_modal_gaussian(5000, means=means, covariances=covariances, weights=[.3, .7])
+#means = [[-1.5, -1.5], [1.5, 1.5]]
+#covariances = [torch.eye(dim)*1.5, torch.eye(dim)*0.5]
+#X_data = sample_modal_gaussian(5000, means=means, covariances=covariances, weights=[.3, .7])
+means = [[0.0, 3.0]]
+covariances = [torch.eye(dim)*1.5]
+X_data = sample_modal_gaussian(5000, means=means, covariances=covariances, weights=[1.0])
 
 fig, axes = plt.subplots(2, 2)
 
 plot_data(axes[0, 0], X_data)
 
 U_data = gdt.X_to_U(X_data)
+#U_data = torch.tensor(
+#    [[0.5, 0.5],
+#    [0.6, 0.5],
+#    [0.7, 0.5],
+#    [0.5, 0.6],
+#    [0.6, 0.6]])
+#print("U data: ", U_data)
 
 plot_data(axes[0, 1], U_data)
+axes[0, 1].set_xlim((0, 1))
+axes[0, 1].set_ylim((0, 1))
 
-#U_data_torch = torch.tensor(U_data, dtype=torch.float32)
-#dataset = TensorDataset(U_data_torch)
-#dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
-#
-#transformer_degrees = [4, 4]
-#conditioner_degrees = [1, 4]
-#model = BernsteinFlowModel(dim=dim, transformer_degrees=transformer_degrees, conditioner_degrees=conditioner_degrees)
-#
-#optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-#
-## Train
-#train(model, dataloader, optimizer, epochs=50)
-#
-#X, Y, Z = evaluate_density_on_grid(model, resolution=100)
-#plot_density(axes[1, 0], X, Y, Z)
+U_data_torch = torch.tensor(U_data, dtype=torch.float32)
+dataset = TensorDataset(U_data_torch)
+dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
+
+transformer_degrees = [10, 10]
+conditioner_degrees = [5, 5]
+model = BernsteinFlowModel(dim=dim, transformer_degrees=transformer_degrees, conditioner_degrees=conditioner_degrees)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+# Train
+train(model, dataloader, optimizer, epochs=100)
+
+X, Y, Z = evaluate_density_on_grid(model, resolution=100)
+plot_density(axes[1, 1], X, Y, Z)
+
+# Show the transformers
+fig2, axes = plt.subplots(dim, 2)
+xi_vals = torch.linspace(0, 1, 100, requires_grad=False)
+for i in range(dim):
+    x_vals = 0.5 * torch.ones(100, dim, requires_grad=False)
+    x_vals[:, i] = xi_vals
+    tf_vals = model.transformer(x_vals, i)
+    tf_deriv_vals = model.transformer_deriv(x_vals, i)
+    axes[i, 0].plot(xi_vals.numpy(), tf_vals.detach().numpy())
+    axes[i, 0].set_ylabel(f"TF {i}")
+    axes[i, 1].plot(xi_vals.numpy(), tf_deriv_vals.detach().numpy())
+    axes[i, 1].set_ylabel(f"dTF/dx {i}")
+
+print("UNCONSTRAINED Model params:")
+for i in range(dim):
+    print(model.A[i])
+
+print("Model params:")
+for i in range(dim):
+    print(model.get_constrained_parameters(i))
+
+#fig3, ax = plot_density_surface(X, Y, Z)
+#ax.set_zlim((0, 1))
 
 
 plt.show()
--- src/bernstein_flow/Model.py
+++ src/bernstein_flow/Model.py
@@ -31,9 +31,11 @@ def bernstein_basis_functions(dim : int, degree : int, coefficients : torch.Tens
         if coefficients is not None:
             B *= coefficients[flat_idx]
 
+        #print("flat idx: ", flat_idx, "b: ", B)
         basis_funcs.append(lambdify(x_syms, B, modules='torch'))
     
-    return basis_funcs, multi_indices, x_syms
+    #print("bais funcs:", len(basis_funcs))
+    return basis_funcs #, multi_indices, x_syms
 
 class BernsteinFlowModel(torch.nn.Module):
     def __init__(self, dim : int, transformer_degrees : int, conditioner_degrees : int):
@@ -44,55 +46,115 @@ class BernsteinFlowModel(torch.nn.Module):
         self.tf_degs = transformer_degrees
         self.cond_degs = conditioner_degrees
 
+        ## Parameters
+        #self.A = torch.nn.ModuleList()
+        #for i in range(dim):
+        #    # Number of coefficients in each conditioner matrix
+        #    alpha_j_size = (conditioner_degrees[i] + 1)**(i)
+
+        #    # Initialize the coefficients of each Bernstein conditioner except the first and last one (which are always 0 and 1)
+        #    A_i = torch.nn.ParameterList([torch.nn.Parameter(torch.rand(alpha_j_size)) for _ in range(transformer_degrees[i] - 1)])
+
+        #    self.A.append(A_i)
+
         # Parameters
-        self.A = torch.nn.ModuleList()
+        #self.A = torch.nn.ModuleList()
+        self.A = torch.nn.ParameterList()
         for i in range(dim):
-            # Number of coefficients in each conditioner matrix
+            # Number of coefficients in each conditioner polynomial (flattened tensor)
             alpha_j_size = (conditioner_degrees[i] + 1)**(i)
 
             # Initialize the coefficients of each Bernstein conditioner except the first and last one (which are always 0 and 1)
-            A_i = torch.nn.ParameterList([torch.nn.Parameter(torch.rand(alpha_j_size)) for _ in range(transformer_degrees[i] - 1)])
+            # Rows correspond to basis polynomials, columns correspond to which TF coefficient the conditioner outputs
+            alpha_matrix = torch.nn.Parameter(torch.rand(alpha_j_size, transformer_degrees[i] - 1))
+        
+            self.A.append(alpha_matrix)
+            #A_i = torch.nn.ParameterList([torch.nn.Parameter(torch.rand(alpha_j_size)) for _ in range(transformer_degrees[i] - 1)])
 
-            self.A.append(A_i)
         
         # Basis functions
         self.tf_basis_funcs = [bernstein_basis_functions(1, transformer_degrees[i]) for i in range(dim)]
         self.tf_deriv_basis_funcs = [bernstein_basis_functions(1, transformer_degrees[i] - 1, scale=transformer_degrees[i]) for i in range(dim)]
-        self.cond_basis_funcs = [bernstein_basis_functions(i + 1, conditioner_degrees[i]) for i in range(dim)]
+        self.cond_basis_funcs = [bernstein_basis_functions(i, conditioner_degrees[i]) for i in range(dim)]
 
     def forward(self, x : torch.Tensor):
         density = torch.ones(x.shape[0], device=x.device)
         for i in range(self.dim):
+            #print("tf deriv basis funcs:", self.tf_deriv_basis_funcs[i])
 
-            # Evaluate the basis functions for each term in the transformer
-            tf_basis_vals = torch.stack([phi_j(x[:, i]) for phi_j in self.tf_deriv_basis_funcs[i]], dim=1)
-
-            # Evaluate the basis functions for each term in the conditioner
-            cond_basis_vals = torch.stack([phi_k(*[x[:, j] for j in range(i)]) for phi_k in self.cond_basis_funcs[i]], dim=1) if i > 0 else torch.ones(x.shape[0], 1, device=x.device)
-
-            cA_i = self.get_constrained_parameters(i)
-
-            tf_coeffs = torch.stack([alpha_j @ cond_basis_vals for alpha_j in cA_i])
-
-            tf_val = tf_coeffs @ tf_basis_vals[1:-1] + tf_basis_vals[-1] # First weight is 0, last weight is 1
-
+            tf_val = self.transformer_deriv(x, i)
             density *= tf_val
         return density
 
-    def get_constrained_parameters(self, dim : int):
-        cA_i = [] # Constrained Ai
-        prev = torch.zeros_like(self.A[dim][0])
-        for alpha_j in self.A[dim]:
-            difference = torch.nn.functional.softplus(alpha_j)
+    def get_constrained_parameters(self, i : int):
+        alpha_matrix = self.A[i]
+        c_alpha_matrix = torch.zeros_like(alpha_matrix) # Constrained alpha matrix
+        prev = torch.zeros_like(alpha_matrix[:, 0])
+        for j in range(alpha_matrix.shape[1]): # Iterate through the vectors of each conditioner component
+            difference = torch.nn.functional.softplus(alpha_matrix[:, j])
             calpha_j = difference + prev
-            cA_i.append(calpha_j)
+            c_alpha_matrix[:, j] = calpha_j
             prev = calpha_j
-        ordered_alpha = torch.stack(cA_i)
-        normalized_alpha = torch.sigmoid(ordered_alpha)
-        return normalized_alpha
+
+        # Normalize all coefficients to be between 0 and 1 (they are already ordered by design)
+        c_alpha_matrix = torch.sigmoid(c_alpha_matrix)
+        return c_alpha_matrix
+    
+    def transformer_deriv(self, x : torch.Tensor, i : int):
+        # Evaluate the basis functions for each term in the transformer
+        #print("number of tf basis funcs", len(self.tf_deriv_basis_funcs[i]))
+        tf_deriv_basis_vals = torch.stack([phi_j(x[:, i]) for phi_j in self.tf_deriv_basis_funcs[i]], dim=1)
+        #print("TF basis vals: \n", [phi_j(x[:, i]) for phi_j in self.tf_deriv_basis_funcs[i]])
+        #print("TF basis vals stacked: \n", tf_deriv_basis_vals)
+
+        # Evaluate the basis functions for each term in the conditioner
+        cond_basis_vals = torch.stack([phi_k(*[x[:, j] for j in range(i)]) for phi_k in self.cond_basis_funcs[i]], dim=1) if i > 0 else torch.ones(x.shape[0], 1, device=x.device)
+        
+
+        c_alpha_matrix = self.get_constrained_parameters(i)
+
+        # Repeat the basis values for each tf coefficient
+        #cond_basis_vals = cond_basis_vals.repeat(1, len(cA_i))
+        
+
+        tf_coeffs = cond_basis_vals @ c_alpha_matrix
+
+        # Compute the difference coefficients used in the derivative of bernstein polynomial (c_{j+1} - c_j)
+        # First weight is 0, last weight is 1
+        coeff_ones = torch.ones(tf_coeffs.shape[0], 1, device=x.device)
+        coeff_zeros = torch.zeros(tf_coeffs.shape[0], 1, device=x.device)
+        tf_deriv_coeffs = torch.cat([tf_coeffs, coeff_ones], dim=1) - torch.cat([coeff_zeros, tf_coeffs], dim=1)
+
+        #print("tf deriv coef: ", tf_deriv_coeffs)
+        #print("tf basis vals: ", tf_deriv_basis_vals)
+        tf_val = torch.sum(torch.mul(tf_deriv_coeffs, tf_deriv_basis_vals), 1) 
+        return tf_val
+    
+    def transformer(self, x : torch.Tensor, i : int):
+        tf_basis_vals = torch.stack([phi_j(x[:, i]) for phi_j in self.tf_basis_funcs[i]], dim=1)
+
+        # Evaluate the basis functions for each term in the conditioner
+        cond_basis_vals = torch.stack([phi_k(*[x[:, j] for j in range(i)]) for phi_k in self.cond_basis_funcs[i]], dim=1) if i > 0 else torch.ones(x.shape[0], 1, device=x.device)
+        
+
+        c_alpha_matrix = self.get_constrained_parameters(i)
+
+        tf_coeffs = cond_basis_vals @ c_alpha_matrix
+
+        # Compute the difference coefficients used in the derivative of bernstein polynomial (c_{j+1} - c_j)
+        # First weight is 0, last weight is 1
+        coeff_ones = torch.ones(tf_coeffs.shape[0], 1, device=x.device)
+        coeff_zeros = torch.zeros(tf_coeffs.shape[0], 1, device=x.device)
+        tf_coeffs = torch.cat([coeff_zeros, tf_coeffs, coeff_ones], dim=1)
+
+        #print("tf deriv coef: ", tf_deriv_coeffs)
+        #print("tf basis vals: ", tf_deriv_basis_vals)
+        tf_val = torch.sum(torch.mul(tf_coeffs, tf_basis_vals), 1) 
+        return tf_val
 
 def nll_loss(model, x_data):
     density = model(x_data)
+    #print("densities: ", density)
     log_density = torch.log(density + 1e-10)
     loss = -log_density.mean()
     return loss
@@ -109,7 +171,7 @@ def train(model : BernsteinFlowModel, data_loader : DataLoader, optimizer, epoch
     for epoch in range(epochs):
         total_loss = 0.0
         for x_batch in data_loader:
-            x_batch = x_batch.to(next(model.parameters()).device)
+            x_batch = x_batch[0].to(next(model.parameters()).device)
             loss = train_step(model, x_batch, optimizer)
             total_loss += loss
         avg_loss = total_loss / len(data_loader)
