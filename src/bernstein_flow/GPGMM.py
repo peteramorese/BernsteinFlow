579c8c574bbafab3a30279da806e375deb8915bb
Peter Amorese <peter.amorese@gmail.com>
2025-11-26 19:09:33 -0700

    Fix gpgmm device bug

--- src/bernstein_flow/GPGMM.py
+++ src/bernstein_flow/GPGMM.py
@@ -117,7 +117,7 @@ class MultivariateGPModel:
             pred = self.likelihood(self.gp(x))
 
             if is_np:
-                return pred.mean.numpy(), pred.covariance_matrix.numpy()
+                return pred.mean.cpu().numpy(), pred.covariance_matrix.cpu().numpy()
             else:
                 return pred.mean, pred.covariance_matrix
     
@@ -144,7 +144,7 @@ class MultivariateGPModel:
             # Reshape to (num_samples, N, T)
             samples = raw_samples.view(n_samples, x.shape[0], -1)
             
-            return samples.numpy() if is_np else samples
+            return samples.cpu().numpy() if is_np else samples
     
     def jacobian(self, x):
         self.gp.eval()
@@ -166,7 +166,7 @@ class MultivariateGPModel:
             return pred.mean.squeeze(0)
 
         J = torch.autograd.functional.jacobian(mean_fcn, x)
-        return J.numpy() if is_np else J
+        return J.cpu().numpy() if is_np else J
 
     def hessian_tensor(self, x):
         """
@@ -192,7 +192,7 @@ class MultivariateGPModel:
             hessian_layers.append(H_i)
 
         H = torch.stack(hessian_layers)
-        return H.numpy() if is_np else H
+        return H.cpu().numpy() if is_np else H
 
 
 def fit_gp(Xp : torch.Tensor, X : torch.Tensor, num_epochs=100, lr=0.1, device='cpu', dtype=torch.float64, print_interval=None):
