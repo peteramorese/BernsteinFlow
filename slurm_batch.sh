3214db37014444ce3d3d4f83605da473eacc6414
Peter Amorese <peter.amorese@gmail.com>
2025-11-29 05:19:34 +0000

    Fix n trials 4D

--- slurm_batch.sh
+++ slurm_batch.sh
@@ -4,6 +4,6 @@
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
-#SBATCH --output=logs/nf_12D.out
+#SBATCH --output=logs/nf_4D.out
 
-./.venv/bin/python3 -u -m scripts.benchmark_12D_ag
+./.venv/bin/python3 -u -m scripts.benchmark_4D
