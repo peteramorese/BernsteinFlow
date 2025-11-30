d252d4823fae9bc245e36a610269a3e3d8286070
Peter Amorese <peter.amorese@gmail.com>
2025-11-30 03:18:28 +0000

    ...

--- slurm_batch.sh
+++ slurm_batch.sh
@@ -4,6 +4,6 @@
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
-#SBATCH --output=logs/nf_6D_nag.out
+#SBATCH --output=logs/sos_12D_ag.out
 
-./.venv/bin/python3 -u -m scripts.benchmark_6D_nag
+./.venv/bin/python3 -u -m scripts.benchmark_12D_ag
